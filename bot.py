a2048394e346b782aeafaa1d3486f708d3274a9b
rehanazmi00009-del <rehanazmi00009@gmail.com>
2026-08-03 04:05:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = 8870339525:AAH5lVVToVR3XgW4i52WNafExWOud4BJt9Q
+TOKEN = "8870339525:AAGdDtbWMggpA4sOPC9Epg133f_d_P9Du0E"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello Asif! Bot chal raha hai.")
+    await update.message.reply_text("Hello Rehan! Bot chal raha hai ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 print("Bot chal raha hai...")
-app.run_polling()8870339525:AAH5lVVToVR3XgW4i52WNafExWOud4BJt9Q
+app.run_polling()
