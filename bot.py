564f356f9e9553de4de67294743b64c007929e9e
rehanazmi00009-del <rehanazmi00009@gmail.com>
2026-08-03 05:33:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# अपना Telegram Bot Token यहाँ डालो
+# अपना BotFather वाला token यहाँ डालो
 TOKEN = "8870339525:AAGdDtbWMggpA4sOPC9Epg133f_d_P9Du0E"
 
 # /start command
@@ -13,19 +13,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # /help command
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Available commands:\n/start - Bot start karo\n/help - Help dekho"
+        "Commands:\n/start - bot start karo\n/help - help dekho"
     )
+
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
-    # Handlers add karo
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
 
     print("Bot started successfully...")
 
-    # Bot start karo
     app.run_polling()
 
 
